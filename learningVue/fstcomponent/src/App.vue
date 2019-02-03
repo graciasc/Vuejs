b872e3b1deb449b122d2fa04a913326c833311b8
Graciasc <Gracias.claude@hotmail.com>
2019-02-03 13:01:16 -0700

    added a template

---
+++ learningVue/fstcomponent/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <information work-experience="I have worked for as  a Software Developer"/>
+  </div>
+</template>
+
+<script>
+import information from './components/Bio.vue'
+
+export default {
+  name: 'app',
+  components: {
+    information
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

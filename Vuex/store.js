240d5375e63f0fddad66aba4cedd803921b4e5a9
Gracias claude <31932887+graciasc@users.noreply.github.com>
2019-03-22 23:33:51 -0600

    Create store.js

---
+++ Vuex/store.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import vue from 'vue'
+import vuex from 'vuex'
+
+Vuex.use(vue)
+
+const store = new store ({
+
+state: {
+state:''
+},
+
+mutations: {
+
+},
+
+actions: {
+
+}
+
+getters: {
+
+}
+
+})
+
+//store boiler
